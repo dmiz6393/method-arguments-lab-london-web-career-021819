f44fcca31523eb4146d37dcd46215909615eb9fc
Danielle Mizrachi <dmiz@brandeis.edu>
2019-02-02 11:36:30 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,10 +1,9 @@
 def introduction(Josh)
   puts "Hi, my name is #{Josh}."
 end
-introduction
+
 
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end 
 
-introduction_with_language
